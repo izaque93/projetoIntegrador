e60482c7dbdc857f9da195f4bc6d2b648683002d
William Rodrigues <willartes@gmail.com>
2022-05-21 20:42:38 -0300

    First commit

---
+++ tarefas/urls.py
@@ -0,0 +1,8 @@
+from tarefas import views
+from django.urls import path
+
+urlpatterns = [
+    path('', views.paginaInicial, name = 'index'),
+    path('About/', views.about, name = 'About'),
+    path('Cause/', views.cause, name = 'Cause')
+]
---
+++ tarefas/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+def paginaInicial(request):
+    return render(request, 'index.html')
+def about(request):
+    return render(request, "About.html")
+
+def cause(request):
+    return render(request, "Cause.html")
