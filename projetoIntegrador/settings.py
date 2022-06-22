3d9e4830b940b75368ab768a9ecff1583b5e1f46
Julio Cartier <c1327805@interno.bb.com.br>
2022-06-22 19:21:58 -0300

    Alterado confguração do projeto

--- projetoIntegrador/settings.py
+++ projetoIntegrador/settings.py
@@ -16,7 +16,8 @@ import django_heroku
 django_heroku.settings(locals())
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+#BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 
@@ -141,10 +142,6 @@ USE_TZ = True
 STATIC_URL = '/staticfiles/'
 #STATIC_ROOT =  'staticfiles'
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'static')
-# ]
-
 STATICFILES_DIRS = [
     os.path.join(PROJECT_ROOT, 'static')
 ]
