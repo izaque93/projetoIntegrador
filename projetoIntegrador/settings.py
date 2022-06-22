769caa9be7a83c30c504b2b133d33d52d821a9a4
Julio Cartier <c1327805@interno.bb.com.br>
2022-06-22 19:26:11 -0300

    Alterado confguração do projeto

--- projetoIntegrador/settings.py
+++ projetoIntegrador/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 import os
 from pathlib import Path
 import django_heroku
-django_heroku.settings(locals())
+django_heroku.settings(locals(), staticfiles=False)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 #BASE_DIR = Path(__file__).resolve().parent.parent
@@ -84,12 +84,6 @@ WSGI_APPLICATION = 'projetoIntegrador.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': BASE_DIR / 'db.sqlite3',
-#    }
-#}
 
 DATABASES = {
     'default': {
