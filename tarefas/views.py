0a610225e495736c4bb601647d2f562502c0a66e
izaque <izaquemalta93@gmail.com>
2022-09-27 10:48:59 -0300

    Remoção do código comentado

--- tarefas/views.py
+++ tarefas/views.py
@@ -9,32 +9,41 @@ import json
 # Create your views here.
 def paginaInicial(request):
     return render(request, 'index.html')
+
+
 def about(request):
     return render(request, "About.html")
+
 
 def cause(request):
     return render(request, "Cause.html")
 
+
 def blog(request):
     return render(request, 'blog.html')
+
 
 def doacoes(request):
     return render(request, "doacoes.html")
 
+
 def pagInscritos(request):
     return render(request, 'account.html')
+
 
 def contaCep(request):
     return render(request, 'accountCep.html')
 
+
 def contato(request):
     return render(request, 'contact.html')
+
 
 def enviaDadosDoacoes(request):
     cadastro = CadastroDoacoes()
     is_ajax = request.headers.get('X-Requested-With') == 'XMLHttpRequest'
     if is_ajax:
-        #data = json.loads(request)
+        # data = json.loads(request)
         roupas = request.GET['roupas']
         num_roupas = request.GET['num_roupas']
         objetos = request.GET['objetos']
@@ -44,10 +53,13 @@ def enviaDadosDoacoes(request):
         brinquedos = request.GET['brinquedos']
         num_brinquedos = request.GET['num_brinquedos']
 
-        retorno = cadastro.inserir_cadastro_doacoes(roupas, num_roupas, objetos, num_objetos, calcados, num_calcados, brinquedos, num_brinquedos)
+        retorno = cadastro.inserir_cadastro_doacoes(roupas, num_roupas, objetos, num_objetos, calcados, num_calcados,
+                                                    brinquedos, num_brinquedos)
 
-        print("Entrouuu 2", roupas, num_roupas, objetos, num_objetos, calcados, num_calcados, brinquedos, num_brinquedos)
+        print("Entrouuu 2", roupas, num_roupas, objetos, num_objetos, calcados, num_calcados, brinquedos,
+              num_brinquedos)
     return JsonResponse({'msg': retorno, 'status': 200}, status=200)
+
 
 def enviarEmailContatos(request):
     cadastro = CadastroDoacoes()
@@ -64,11 +76,12 @@ def enviarEmailContatos(request):
 
     return JsonResponse({'msg': "mensagem", 'status': 200}, status=200)
 
+
 def cadastroComCep(request):
     cadastro = CadastroDoacoes()
     is_ajax = request.headers.get('X-Requested-With') == 'XMLHttpRequest'
     if is_ajax:
-        #data = json.loads(request)
+        # data = json.loads(request)
         cep = request.GET['cep']
         rua = request.GET['rua']
         numero = request.GET['numero']
@@ -87,6 +100,7 @@ def cadastroComCep(request):
             retorno = cadastro.cadastroComCep(cep, rua, numero, complemento, bairro, cidade, uf, email, password)
             print("Cadastrado ", cep, rua, numero, complemento, bairro, cidade, uf, email, password)
             return JsonResponse({'msg': "mensagem", 'status': 200}, status=200)
+
 
 def entrarIndex(request):
     cadastro = CadastroDoacoes()
